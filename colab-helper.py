0c2191a251aa8f68c0e5fcdda01ea5a015db3217
AsaBarthMaron <asabarthmaron@gmail.com>
2020-06-21 16:06:06 -0400

    Minor fixes

--- colab-helper.py
+++
@@ -1,6 +0,0 @@
-def worksheet_to_df(worksheet, data_inds):
-  df = worksheet.get_all_values()
-  df = pd.DataFrame(df)
-  df.columns = df.iloc[0] # Assumes column names are in first row of worksheet.
-  df = df.iloc[data_inds] # Keep only the data
-  return df
